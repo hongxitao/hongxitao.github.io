53b4170a112f685e5c2f4c3eed21867ecf393447
hongxitao <48063141+hongxitao@users.noreply.github.com>
2022-08-03 23:24:24 +0100

    range test

--- weather.js
+++ weather.js
@@ -110,18 +110,19 @@ AFRAME.registerComponent('playground', {
   let check;
 
   marker.addEventListener('markerFound', () => {
-      let cameraPosition = camera.object3D.position;
-      let markerPosition = marker.object3D.position;
-      let distance = cameraPosition.distanceTo(markerPosition)
+    alert("found it");
+      // let cameraPosition = camera.object3D.position;
+      // let markerPosition = marker.object3D.position;
+      // let distance = cameraPosition.distanceTo(markerPosition)
 
-      check = setInterval(() => {
-          cameraPosition = camera.object3D.position;
-          markerPosition = marker.object3D.position;
-          distance = cameraPosition.distanceTo(markerPosition)
+      // check = setInterval(() => {
+      //     cameraPosition = camera.object3D.position;
+      //     markerPosition = marker.object3D.position;
+      //     distance = cameraPosition.distanceTo(markerPosition)
 
-          // do what you want with the distance:
-          console.log(distance);
-      }, 100);
+      //     // do what you want with the distance:
+      //     console.log(distance);
+      // }, 100);
   });
 
   marker.addEventListener('markerLost', () => {
