5f87af6dfb8ba268f353f87d1728ced735435c7a
hongxitao <48063141+hongxitao@users.noreply.github.com>
2022-08-06 21:33:57 +0100

    Update loc_and_marker.js

--- loc_and_marker.js
+++ loc_and_marker.js
@@ -10,15 +10,19 @@ AFRAME.registerComponent('range_test', {
             marker.addEventListener("markerFound", function(){
                 //let cameraPosition = camera.object3D.position;
                 let markerPosition = marker.object3D.position;
+                let markerRotation = marker.object3D.rotation;
                 //console.log(cameraPosition);
-                console.log(markerPosition);
+                //console.log(markerRotation._x);
                 text.setAttribute("value", "found");
 
                 update = setInterval(() => {
                     //cameraPosition = camera.object3D.position;
                     markerPosition = marker.object3D.position;
+                    markerRotation = marker.object3D.rotation;
+                    //box.object3D.position.set(markerPosition);
+                    box.object3D.setRotationFromEuler(markerRotation);
                     box.setAttribute("position",{x:markerPosition.x, y:markerPosition.y, z:markerPosition.z});
-        
+                    //box.setAttribute("rotation",{x:markerRotation._x, y:markerRotation._y, z:markerRotation._z})
                     // do what you want with the distance:
                     //console.log(distance);
                 }, 100);
@@ -26,6 +30,9 @@ AFRAME.registerComponent('range_test', {
             })
             marker.addEventListener("markerLost", function(){
                 text.setAttribute("value", "lost");
+                box.setAttribute("position", {x:0, y:1, z:-8});
+                console.log(box.object3D.position);
+                clearInterval(update);
 
             })
 
@@ -67,5 +74,9 @@ AFRAME.registerComponent('range_test', {
         //     })
         // })
 
+    },
+
+    tick: function(){
+        console.log(document.querySelector("a-box").object3D.position);
     }
 })
