6a4f8b9cb83cf73a03f3d81050b39ecb63e146e0
hongxitao <48063141+hongxitao@users.noreply.github.com>
2022-08-07 22:13:48 +0100

    gyh

--- loc_and_marker.js
+++ loc_and_marker.js
@@ -8,6 +8,7 @@ AFRAME.registerComponent('range_test', {
             const box = document.querySelector('a-box');
             let markerPosition;
             let markerRotation;
+            let boxPostion; 
             let update;
             marker.addEventListener("markerFound", function(){
                 //let cameraPosition = camera.object3D.position;
@@ -20,9 +21,10 @@ AFRAME.registerComponent('range_test', {
                     //cameraPosition = camera.object3D.position;
                     markerPosition = marker.object3D.position;
                     markerRotation = marker.object3D.rotation;
+                    boxPostion = box.object3D.position;
                     //box.object3D.position.set(markerPosition);
-                    box.object3D.setRotationFromEuler(markerRotation);
-                    box.setAttribute("position",{x:markerPosition.x, y:markerPosition.y, z:markerPosition.z});
+                    // box.object3D.setRotationFromEuler(markerRotation);
+                    // box.setAttribute("position",{x:markerPosition.x, y:markerPosition.y, z:markerPosition.z});
                     //box.setAttribute("rotation",{x:markerRotation._x, y:markerRotation._y, z:markerRotation._z})
                     // do what you want with the distance:
                     //console.log(distance);
@@ -31,7 +33,7 @@ AFRAME.registerComponent('range_test', {
             })
             marker.addEventListener("markerLost", function(){
                 text.setAttribute("value", "lost");
-                box.setAttribute("position", {x:markerPosition.x, y:markerPosition.y, z:markerPosition.z});
+                box.setAttribute("position", {x:markerPosition.x+boxPostion.x, y:markerPosition.y+boxPostion.y, z:markerPosition.z+boxPostion.z});
                 //console.log(box.object3D.position);
                 clearInterval(update);
 
