82a96a43a7aeca83900ae4235e7bba0d97352d3e
hongxitao <48063141+hongxitao@users.noreply.github.com>
2022-08-10 19:48:44 +0100

    asddas

--- loc_and_marker.js
+++ loc_and_marker.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('range_test', {
             let width = window.screen.availWidth;
             let height = window.screen.availHeight;
             
-            scene.setAttribute("arjs", `trackingMethod: best; sourceType: webcam; debugUIEnabled: false; sourceWidth:${width};sourceHeight:${height};displayWidth::${width}; displayHeight::${height}; maxDetectionRate: 60;`)
+            scene.setAttribute("arjs", `trackingMethod: best; sourceType: webcam; debugUIEnabled: false; sourceWidth:${width};sourceHeight:${height};displayWidth::${width}; displayHeight::${height}; `)
             const marker = document.querySelector("a-marker");
             
             var text = document.querySelector("#top");
